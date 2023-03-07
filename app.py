439724e2e9e84667af693623875a1050f02b4697
diawando <abduldiawade777@gmail.com>
2023-03-07 15:31:44 +0000

    setting a mongo db user and pw

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+client = MongoClient('localhost', 27017, username="AdminDiawara", password="Dieuestbon")
 
 db = client.flask_db
 
